3a4cab6d2026bbd65747507612777e295036b324
Mohamed Mousa <mohamed.mousa.dev@gmail.com>
2024-04-19 18:39:18 +0400

    video modal

--- src/pages/Home/videoModel.jsx
+++ src/pages/Home/videoModel.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import ModalVideo from "react-modal-video";
 import axios from "axios";
 import address from "../../services";
-import getVideoId from 'get-video-id';
+import getVideoId from "get-video-id";
 
 class ModalVedioComponent extends Component {
   constructor() {
@@ -12,7 +12,7 @@ class ModalVedioComponent extends Component {
       isOpen: false,
       link: " ",
       video: [],
-      id: ""
+      id: "",
     };
     this.openModal = this.openModal.bind(this);
   }
@@ -27,15 +27,13 @@ class ModalVedioComponent extends Component {
         headers: { "accept-language": `en` },
       });
       this.setState({ video });
-      this.setState({ link: video.link })
-      this.setState({ id: getVideoId(this.state.link) })
+      this.setState({ link: video.link });
+      this.setState({ id: getVideoId(this.state.link) });
 
       console.log("the video link  ", this.state.link);
     } catch (error) {
       console.log("can not load video ...........");
     }
-
-
   }
   // get projct id from url
   async componentWillReceiveProps() {
@@ -44,47 +42,68 @@ class ModalVedioComponent extends Component {
         headers: { "accept-language": `en` },
       });
       this.setState({ video });
-      this.setState({ link: video.link })
-      this.setState({ id: getVideoId(this.state.link) })
-
+      this.setState({ link: video.link });
+      this.setState({ id: getVideoId(this.state.link) });
     } catch (error) {
       console.log("can not load Contact for the contact page slider");
     }
-
   }
 
-
   render() {
-    let link = this.state.link
-    const { id } = getVideoId(link)
+    let link = this.state.link;
+    const { id } = getVideoId(link);
     //console.log("here we go this is the youtube testttttttted link ********",link)
     //console.log("here we go this is the youtube id ********",id)
 
-
     return (
+      <main>
+      {this.state.isOpen && 
+          <>
 
-      <main className="overflow-hidden">
-        {id != "" ?
-          <ModalVideo
-
-            channel="youtube"
-            isOpen={this.state.isOpen}
-            videoId={id}
-            onClose={() => this.setState({ isOpen: false })}
-          /> :
-          <ModalVideo
-            channel="youtube"
-            isOpen={this.state.isOpen}
-            videoId="JI-1UEwo-tg"
-            onClose={() => this.setState({ isOpen: false })}
-          />
-        }
+        {id != "" ? (
+          <div 
+          className="fixed z-[9999999999999] h-screen inset-0 bg-black">
+            <ModalVideo
+              channel="youtube"
+              isOpen={this.state.isOpen}
+              videoId={id}
+              onClose={() => this.setState({ isOpen: false })}
+            />
+          </div>
+        ) : (
+          <div 
+          className="fixed z-[9999999999999] h-screen inset-0 bg-black">
+            <ModalVideo
+              channel="youtube"
+              isOpen={this.state.isOpen}
+              videoId="JI-1UEwo-tg"
+              onClose={() => this.setState({ isOpen: false })}
+            />
+          </div>
+        )}
+            
+          </>
+          }
 
         <a onClick={this.openModal} className="modal-btn">
-          <div className="pt-10 pb-10 grid grid-cols-9 justify-content-center " >
+          <div className="pt-10 pb-10 grid grid-cols-9 justify-content-center ">
             <div className="col-span-4 cursor-pointer"></div>
-            <svg className="cursor-pointer w-24 h-24 p-3 col-span-1 rounded-full border-2 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 18V6l8 6-8 6Z" />
+            <svg
+              className="cursor-pointer w-24 h-24 p-3 col-span-1 rounded-full border-2 text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 18V6l8 6-8 6Z"
+              />
             </svg>
           </div>
         </a>
